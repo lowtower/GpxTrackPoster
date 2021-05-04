a791426b730e7242cce1fdcc4eafe092efe88b9b
Lowtower <lowtower@gmx.de>
2021-05-04 10:37:04 +0200

    add examples

--- examples/examples.sh
+++ examples/examples.sh
@@ -13,7 +13,7 @@ done
 
 exit 
 
-for TYPE in grid calendar circular heatmap ; do
+for TYPE in grid calendar circular heatmap clock ; do
     ../create_poster.py --gpx-dir ../2016-freiburg --year 2016 \
         --athlete "Florian Pigorsch" --title "My Runs 2016 (Freiburg Area)" \
         --type $TYPE --output example_$TYPE.svg \
