ab6bd72a2f4e5d012a624644659dee1c5f940074
Lowtower <lowtower@gmx.de>
2021-05-06 17:26:59 +0200

    add rounding method rename int method

--- tests/test_xy.py
+++ tests/test_xy.py
@@ -10,66 +10,66 @@ from gpxtrackposter.xy import XY
 
 def test_multiplication() -> None:
     test_object: XY = XY(50.0, 100.0)
-    test_values: List[Union[Tuple[float, XY], Tuple[XY, XY]]] = [
-        (10.0, XY(500.0, 1000.0)),
-        (10, XY(500.0, 1000.0)),
-        (0.5, XY(25.0, 50.0)),
-        (-5.0, XY(-250.0, -500.0)),
-        (XY(10.0, 5.0), XY(500.0, 500.0)),
-        (XY(-10.0, 5.0), XY(-500.0, 500.0)),
-        (XY(0.5, -5.0), XY(25.0, -500.0)),
+    test_values: List[Union[Tuple[float, Tuple[float, float]], Tuple[XY, Tuple[float, float]]]] = [
+        (10.0, (500.0, 1000.0)),
+        (10, (500.0, 1000.0)),
+        (0.5, (25.0, 50.0)),
+        (-5.0, (-250.0, -500.0)),
+        (XY(10.0, 5.0), (500.0, 500.0)),
+        (XY(-10.0, 5.0), (-500.0, 500.0)),
+        (XY(0.5, -5.0), (25.0, -500.0)),
     ]
     for test_value in test_values:
         other, result = test_value
-        assert (test_object * other).tuple() == result.tuple()
+        assert (test_object * other).tuple() == result
 
 
 def test_division() -> None:
     test_object: XY = XY(50.0, 100.0)
-    test_values: List[Union[Tuple[float, XY], Tuple[XY, XY]]] = [
-        (10.0, XY(5.0, 10.0)),
-        (10, XY(5.0, 10.0)),
-        (0.5, XY(100.0, 200.0)),
-        (-5.0, XY(-10.0, -20.0)),
-        (XY(10.0, 5.0), XY(5.0, 20.0)),
-        (XY(-10.0, 5.0), XY(-5.0, 20.0)),
-        (XY(0.5, -5.0), XY(100.0, -20.0)),
+    test_values: List[Union[Tuple[float, Tuple[float, float]], Tuple[XY, Tuple[float, float]]]] = [
+        (10.0, (5.0, 10.0)),
+        (10, (5.0, 10.0)),
+        (0.5, (100.0, 200.0)),
+        (-5.0, (-10.0, -20.0)),
+        (XY(10.0, 5.0), (5.0, 20.0)),
+        (XY(-10.0, 5.0), (-5.0, 20.0)),
+        (XY(0.5, -5.0), (100.0, -20.0)),
     ]
     for test_value in test_values:
         other, result = test_value
-        assert (test_object / other).tuple() == result.tuple()
+        assert (test_object / other).tuple() == result
 
 
 def test_addition() -> None:
     test_object: XY = XY(50.0, 100.0)
-    test_values: List[Union[Tuple[float, XY], Tuple[XY, XY]]] = [
-        (10.0, XY(60.0, 110.0)),
-        (10, XY(60.0, 110.0)),
-        (0.5, XY(50.5, 100.5)),
-        (-5.0, XY(45.0, 95.0)),
-        (XY(10.0, 5.0), XY(60.0, 105.0)),
-        (XY(-10.0, 5.0), XY(40.0, 105.0)),
-        (XY(0.5, -5.0), XY(50.5, 95.0)),
+    test_values: List[Union[Tuple[float, Tuple[float, float]], Tuple[XY, Tuple[float, float]]]] = [
+        (10.0, (60.0, 110.0)),
+        (10, (60.0, 110.0)),
+        (0.5, (50.5, 100.5)),
+        (-5.0, (45.0, 95.0)),
+        (XY(10.0, 5.0), (60.0, 105.0)),
+        (XY(-10.0, 5.0), (40.0, 105.0)),
+        (XY(0.5, -5.0), (50.5, 95.0)),
     ]
     for test_value in test_values:
         other, result = test_value
-        assert (test_object + other).tuple() == result.tuple()
+        assert (test_object + other).tuple() == result
 
 
 def test_subtraction() -> None:
     test_object: XY = XY(50.0, 100.0)
-    test_values: List[Union[Tuple[float, XY], Tuple[XY, XY]]] = [
-        (10.0, XY(40.0, 90.0)),
-        (10, XY(40.0, 90.0)),
-        (0.5, XY(49.5, 99.5)),
-        (-5.0, XY(55.0, 105.0)),
-        (XY(10.0, 5.0), XY(40.0, 95.0)),
-        (XY(-10.0, 5.0), XY(60.0, 95.0)),
-        (XY(0.5, -5.0), XY(49.5, 105.0)),
+    test_values: List[Union[Tuple[float, Tuple[float, float]], Tuple[XY, Tuple[float, float]]]] = [
+        (10.0, (40.0, 90.0)),
+        (10, (40.0, 90.0)),
+        (0.5, (49.5, 99.5)),
+        (-5.0, (55.0, 105.0)),
+        (XY(10.0, 5.0), (40.0, 95.0)),
+        (XY(-10.0, 5.0), (60.0, 95.0)),
+        (XY(0.5, -5.0), (49.5, 105.0)),
     ]
     for test_value in test_values:
         other, result = test_value
-        assert (test_object - other).tuple() == result.tuple()
+        assert (test_object - other).tuple() == result
 
 
 def test_representation() -> None:
@@ -82,28 +82,44 @@ def test_tuple() -> None:
     assert test_object.tuple() == (50.0, 100.0)
 
 
-def test_int() -> None:
+def test_to_int() -> None:
     test_object: XY = XY(50.12345, 100.98765)
-    assert test_object.int().tuple() == (50, 100)
+    assert test_object.to_int().tuple() == (50, 100)
+
+
+def test_round() -> None:
+    test_object: XY = XY(50.12345, 100.98765)
+    test_values: List[Union[Tuple[int, Tuple[float, float]], Tuple[None, Tuple[float, float]]]] = [
+        (0, (50.0, 101.0)),
+        (1, (50.1, 101.0)),
+        (2, (50.12, 100.99)),
+        (3, (50.123, 100.988)),
+        (4, (50.1234, 100.9877)),
+        (5, (50.12345, 100.98765)),
+        (None, (50.0, 101.0)),
+    ]
+    for test_value in test_values:
+        n, result = test_value
+        assert test_object.round(n).tuple() == result
 
 
 def test_scale_to_max_value() -> None:
     test_object: XY = XY(50.0, 100.0)
-    good_values: List[Tuple[float, XY]] = [
-        (25.0, XY(12.5, 25.0)),
-        (50.0, XY(25.0, 50.0)),
-        (200.0, XY(100.0, 200.0)),
-        (-50.0, XY(-25.0, -50.0)),
+    good_values: List[Tuple[float, Tuple[float, float]]] = [
+        (25.0, (12.5, 25.0)),
+        (50.0, (25.0, 50.0)),
+        (200.0, (100.0, 200.0)),
+        (-50.0, (-25.0, -50.0)),
     ]
-    bad_values: List[Tuple[float, XY]] = [
-        (25.0, XY(25.0, 12.5)),
-        (-50.0, XY(25.0, -50.0)),
-        (-50.0, XY(-25.0, 50.0)),
+    bad_values: List[Tuple[float, Tuple[float, float]]] = [
+        (25.0, (25.0, 12.5)),
+        (-50.0, (25.0, -50.0)),
+        (-50.0, (-25.0, 50.0)),
     ]
     for good_value in good_values:
         max_value, result = good_value
-        assert test_object.scale_to_max_value(max_value).tuple() == result.tuple()
+        assert test_object.scale_to_max_value(max_value).tuple() == result
 
     for bad_value in bad_values:
         max_value, result = bad_value
-        assert test_object.scale_to_max_value(max_value).tuple() != result.tuple()
+        assert test_object.scale_to_max_value(max_value).tuple() != result
